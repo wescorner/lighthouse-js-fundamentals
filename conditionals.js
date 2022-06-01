7c36aee62c19d6e183447efa5e4689cc9bd65c2c
Wesley Corner <wescorner@gmail.com>
2022-06-01 23:30:49 +0000

    More conditionals

--- conditionals.js
+++ conditionals.js
@@ -1,13 +1,14 @@
 const temperature = 12;
+if(temperature < -40 || temperature > 40){
+  console.log("Maybe going out isn't such a great idea");
+}
 if(temperature < 0){
   console.log("Make sure you pick out a scarf!");
 }
 else if(temperature < 15){
   console.log("Short sleeves won't cut it!");
 }
-
 else{
   console.log("Short sleeves are fine!");
 }
-
-console.log("Now you're ready to go outside!");
+console.log("Now you're ready to go outside!"); 
